95b07bbcf2b888ad95e13b4ac5ac06bb674d0065
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2026-08-15 11:06:56 +0200

    add todo relationships

--- app/models/todo.ts
+++ app/models/todo.ts
@@ -1,4 +1,9 @@
 import { TodoSchema } from '#database/schema'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import User from '#models/user'
+import { belongsTo } from '@adonisjs/lucid/orm'
 
 export default class Todo extends TodoSchema {
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
 }
--- app/models/user.ts
+++ app/models/user.ts
@@ -2,6 +2,9 @@ import { UserSchema } from '#database/schema'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { type HasMany } from '@adonisjs/lucid/types/relations'
+import Todo from '#models/todo'
+import { hasMany } from '@adonisjs/lucid/orm'
 
 /**
  * User model represents a user in the application.
@@ -9,6 +12,9 @@ import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
  * through the withAuthFinder mixin.
  */
 export default class User extends compose(UserSchema, withAuthFinder(hash)) {
+  @hasMany(() => Todo)
+  declare todos: HasMany<typeof Todo>
+
   /**
    * Get the user's initials from their full name or email.
    * Returns the first letter of first and last name if available,
